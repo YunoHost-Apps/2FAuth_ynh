0872b3a9d893280129d846f8900d421437e560d5
ericgaspar <junk.eg@free.fr>
2022-03-25 18:30:41 +0100

    First commit

---
+++ doc/DESCRIPTION.md
@@ -0,0 +1,11 @@
+2FAuth is a web based self-hosted alternative to One Time Passcode (OTP) generators like Google Authenticator, designed for both mobile and desktop.
+
+It aims to ease you perform your 2FA authentication steps whatever the device you handle, with a clean and suitable interface.
+
+### Features
+
+- Manage your 2FA accounts and organize them using Groups
+- Scan and decode any QR code to add account in no time
+- Add custom account without QR code thanks to an advanced form
+- Edit accounts, even the imported ones
+- Generate TOTP and HOTP security codes
---
+++ scripts/_common.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#=================================================
+# COMMON VARIABLES
+#=================================================
+
+YNH_PHP_VERSION="7.3"
+
+# Composer version
+YNH_COMPOSER_VERSION="2.1.3"
+
+pkg_dependencies="php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysql"
+
+#=================================================
+# PERSONAL HELPERS
+#=================================================
+
+#=================================================
+# EXPERIMENTAL HELPERS
+#=================================================
+
+#=================================================
+# FUTURE OFFICIAL HELPERS
+#=================================================
