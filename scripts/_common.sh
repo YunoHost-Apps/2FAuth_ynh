70cf2ec50a712765e55a2ff6bd33e1c933345838
ericgaspar <junk.eg@free.fr>
2022-04-07 15:59:53 +0200

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,9 +7,9 @@
 YNH_PHP_VERSION="7.3"
 
 # Composer version
-YNH_COMPOSER_VERSION="2.1.3"
+YNH_COMPOSER_VERSION="2.3.3"
 
-pkg_dependencies="php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysql"
+pkg_dependencies=" php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysql"
 
 #=================================================
 # PERSONAL HELPERS
