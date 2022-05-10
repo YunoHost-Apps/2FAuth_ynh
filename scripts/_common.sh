83270614bc3a930595578bacd04e3be685913a1c
ericgaspar <junk.eg@free.fr>
2022-05-10 08:45:44 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,12 +4,12 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.4"
+YNH_PHP_VERSION="8.0"
 
 # Composer version
-YNH_COMPOSER_VERSION="2.3.3"
+YNH_COMPOSER_VERSION="2.3.5"
 
-pkg_dependencies="php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysql"
+pkg_dependencies="php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysql"
 
 #=================================================
 # PERSONAL HELPERS
